53ced825db087717064219d207073274a53db934
Bhavesh Nikam <96333025+bhaveshnikam@users.noreply.github.com>
2023-02-21 19:51:09 +0530

    Create potd5.cpp

---
+++ potd5.cpp
@@ -0,0 +1,16 @@
+class Solution{   
+public:
+    int minIteration(int N, int M, int x, int y){    
+        // code here
+        int ans=0;
+        for(int i=1; i<=N; i++)
+        {
+            for(int j=1; j<=M; j++)
+            {
+                ans=max(ans,abs(i-x)+abs(j-y));
+            }
+        }
+        
+        return ans;
+    }
+};
